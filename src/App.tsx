8e22d99893832ea59d5962aee60d272effc82039
nmegrant <nme.grant@gmail.com>
2020-08-31 12:24:57 +0200

    Spent way too long getting the dates to work

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import MapChart from "./Components/MapChart";
 function App() {
   return (
     <div className="App">
+      <h1>Moonshot Calendar</h1>
       <MapChart />
     </div>
   );
--- src/Components/MapChart.tsx
+++ src/Components/MapChart.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
   ComposableMap,
   Geographies,
@@ -6,6 +7,8 @@ import {
   Marker,
   Point,
 } from "react-simple-maps";
+import Spinner from "react-bootstrap/Spinner";
+import moment from "moment";
 
 interface Markers {
   markerOffset: number;
@@ -16,42 +19,41 @@ interface Markers {
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-const markers: Markers[] = [
-  {
-    markerOffset: -15,
-    name: "Buenos Aires",
-    coordinates: [-58.3816, -34.6037],
-  },
-  { markerOffset: -15, name: "La Paz", coordinates: [-68.1193, -16.4897] },
-  { markerOffset: 25, name: "Brasilia", coordinates: [-47.8825, -15.7942] },
-  { markerOffset: 25, name: "Santiago", coordinates: [-70.6693, -33.4489] },
-  { markerOffset: 25, name: "Bogota", coordinates: [-74.0721, 4.711] },
-  { markerOffset: 25, name: "Quito", coordinates: [-78.4678, -0.1807] },
-  { markerOffset: -15, name: "Georgetown", coordinates: [-58.1551, 6.8013] },
-  { markerOffset: -15, name: "Asuncion", coordinates: [-57.5759, -25.2637] },
-  { markerOffset: 25, name: "Paramaribo", coordinates: [-55.2038, 5.852] },
-  { markerOffset: 25, name: "Montevideo", coordinates: [-56.1645, -34.9011] },
-  { markerOffset: -15, name: "Caracas", coordinates: [-66.9036, 10.4806] },
-  { markerOffset: -15, name: "Lima", coordinates: [-77.0428, -12.0464] },
-];
-
 export default function MapChart() {
+  let markers: Markers[] = [];
+  const [points, setPoints] = useState(markers);
+
+  //get current date
+  let currentDate: string = moment().format("YYYY-MM-DD");
+  let futureMonth = moment().add(3, "months").format("YYYY-MM-DD");
+
+  useEffect(() => {
+    const fetchLaunchDate = async () => {
+      try {
+        const response = await axios.get(
+          `https://launchlibrary.net/1.3/launch/${currentDate}/${futureMonth}/?limit=100`
+        );
+        console.log(response.data.launches);
+        markers = response.data.launches.map((l: any) => {
+          return {
+            markerOffset: 15,
+            name: l.name,
+            coordinates: [
+              l.location.pads[0].longitude,
+              l.location.pads[0].latitude,
+            ],
+          };
+        });
+        setPoints(markers);
+      } catch (error) {
+        console.log(`Error getting launch data: ${error}`);
+      }
+    };
+    fetchLaunchDate();
+  }, []);
+
   return (
     <ComposableMap>
-      {/* <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies
-            // .filter((d) => d.properties.REGION_UN === "Americas")
-            .map((geo) => (
-              <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                fill="#EAEAEC"
-                stroke="#D6D6DA"
-              />
-            ))
-        }
-      </Geographies> */}
       <Geographies geography={geoUrl}>
         {({ geographies }) =>
           geographies.map((geo) => (
@@ -59,18 +61,22 @@ export default function MapChart() {
           ))
         }
       </Geographies>
-      {markers.map(({ name, coordinates, markerOffset }) => (
-        <Marker key={name} coordinates={coordinates}>
-          <circle r={10} fill="#F00" stroke="#fff" strokeWidth={2} />
-          <text
-            textAnchor="middle"
-            y={markerOffset}
-            style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
-          >
-            {name}
-          </text>
-        </Marker>
-      ))}
+      {points ? (
+        points.map(({ name, coordinates, markerOffset }) => (
+          <Marker key={name} coordinates={coordinates}>
+            <circle r={10} fill="#F00" stroke="#fff" strokeWidth={2} />
+            <text
+              textAnchor="middle"
+              y={markerOffset}
+              style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+            >
+              {name}
+            </text>
+          </Marker>
+        ))
+      ) : (
+        <Spinner animation="border" />
+      )}
     </ComposableMap>
   );
 }
